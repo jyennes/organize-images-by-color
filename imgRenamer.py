5f60dc9cff07f16e81ffdd6d7eedbf790b210c83
jyennes <jyennes5@gmail.com>
2022-03-03 17:52:54 -0500

    hue range function
    - added function to select color based on hue value
    - small adjustments

--- imgRenamer.py
+++ imgRenamer.py
@@ -5,24 +5,55 @@
 # import
 from colorthief import ColorThief
 from matplotlib import colors
+import os
 
-# Detect dominant color from image
-ct = ColorThief('redcircle.jpg')
-dominant_color = ct.get_color(quality=10)
-print ("Dominant color: ", dominant_color)
+def clrDetect(image):
+    # Detect dominant color from image
+    ct = ColorThief(image)
+    dominant_color = ct.get_color(quality=10)
+    print ("Dominant color: ", dominant_color)
 
-## convert RGB to HSV
-# scale RGB format from 255 to 1
-rgb = list()
-for x in range(len(dominant_color)):
-    rgb.append(dominant_color[x] / 255)  
-print ("RGB: ", rgb)
+    ## convert RGB to HSV
+    # scale RGB format from 255 to 1
+    rgb = list()
+    for x in range(len(dominant_color)):
+        rgb.append(dominant_color[x] / 255)  
+    print ("RGB: ", rgb)
 
-hsv = colors.rgb_to_hsv(rgb)
-# scale hue value to 360
-hsv[0] = hsv[0] * 360
-print ("HSV: ", hsv)
+    hsv = colors.rgb_to_hsv(rgb)
+    # scale hue value to 360
+    hsv[0] = hsv[0] * 360
+    print ("HSV: ", hsv)
 
+# test output
+# image = 'blue.jpg'
+# clrDetect(image)
+
+# Create groups by hue range
+def getColor(hue):
+    # Red
+    if hue >= 0 and hue <= 60: 
+        domColor = 'red'
+    # Yellow
+    elif hue >= 61 and hue <= 120: 
+        domColor = 'Yellow'
+    # Green
+    elif hue >= 121 and hue <= 180: 
+        domColor = 'Green'
+    # Cyan
+    elif hue >= 181 and hue <= 240: 
+        domColor = 'Cyan'
+    # Blue
+    elif hue >= 241 and hue <= 300: 
+        domColor = 'Blue'
+    # Magenta
+    elif hue >= 301 and hue <= 360: 
+        domColor = 'Magenta'
+
+    return domColor
 
 #TODO rename files based on dominant color
-# https://docs.microsoft.com/en-us/windows/python/scripting
+# prepend to file names instead of replace
+# os.rename('red.jpg', 'blue.jpg')
+
+
