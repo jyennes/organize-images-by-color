5d55a85fe09ba9d5813df5c733b1d88d10cf4383
jyennes <jyennes5@gmail.com>
2022-03-07 19:12:38 -0500

    first working version
    - works while being slow and not fully accurate 
    - added function to rename file
    - added function to select directory (not implemented)
    - added top level code

--- imgRenamer.py
+++ imgRenamer.py
@@ -6,24 +6,28 @@
 from colorthief import ColorThief
 from matplotlib import colors
 import os
+from tkinter import *
 
 def clrDetect(image):
     # Detect dominant color from image
     ct = ColorThief(image)
-    dominant_color = ct.get_color(quality=10)
-    print ("Dominant color: ", dominant_color)
+    dominant_color = ct.get_color(quality=1)
+    # print ("Dominant color: ", dominant_color)
 
     ## convert RGB to HSV
     # scale RGB format from 255 to 1
     rgb = list()
     for x in range(len(dominant_color)):
         rgb.append(dominant_color[x] / 255)  
-    print ("RGB: ", rgb)
+    # print ("RGB: ", rgb)
 
     hsv = colors.rgb_to_hsv(rgb)
     # scale hue value to 360
     hsv[0] = hsv[0] * 360
-    print ("HSV: ", hsv)
+    # print ("HSV: ", hsv)
+
+    # return hue value
+    return hsv[0]
 
 # test output
 # image = 'blue.jpg'
@@ -33,7 +37,7 @@ def clrDetect(image):
 def getColor(hue):
     # Red
     if hue >= 0 and hue <= 60: 
-        domColor = 'red'
+        domColor = 'Red'
     # Yellow
     elif hue >= 61 and hue <= 120: 
         domColor = 'Yellow'
@@ -52,8 +56,41 @@ def getColor(hue):
 
     return domColor
 
-#TODO rename files based on dominant color
+## rename files based on dominant color
+def fileNamer(path, name, color):
+    os.chdir(path)
+    os.rename(name, (color + name))
 # prepend to file names instead of replace
 # os.rename('red.jpg', 'blue.jpg')
 
+## select directory
+# dir = input("Enter directory: ")
+# root = Tk()
+# root.withdraw()
+# path = filedialog.askdirectory()
+# print (path)
 
+#temp path
+path = r"G:\Pictures\1080p"
+absp = os.path.abspath(path)
+tempImg = os.listdir(path)[1]
+# fullPath = absp + "\\" + tempImg
+# print (fullPath)
+# print (clrDetect(fullPath))
+
+# loop through directory
+n = 0
+while n < len(os.listdir(path))+1:
+    img = os.listdir(path)[n]
+    fullPath = absp + "\\" + img
+    hue = clrDetect(fullPath)
+    color = getColor(hue)
+    fileNamer(path, img, color)
+    n+=1
+
+
+###
+#TODO select directory
+#TODO adjust file order used by os.listdir
+#TODO more accurate colorthief
+###
